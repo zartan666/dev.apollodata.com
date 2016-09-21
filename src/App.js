151f65a0c3144087febb08f5f50c331f10d549ad
Tom Coleman <tom@thesnail.org>
2016-09-21 17:39:43 +1000

    Added some pitch text

--- src/App.js
+++ src/App.js
@@ -15,11 +15,11 @@ export default class App extends Component {
     return (
       <div>
         <h2 className="headline-section">Query Example</h2>
-        <div className="desc-section">XXX: Pitch why Apollo is awesome for querying data.</div>
+        <div className="desc-section">Apollo makes fetching the exact data you need for your component easy and allows you to put your queries exactly where you need them.</div>
         <CodeSnippet operation="query" activeLibrary={library} onLibraryChange={this.onLibraryChange} />
 
         <h2 className="headline-section">Mutation Example</h2>
-        <div className="desc-section">XXX: Pitch why Apollo is awesome for mutating data.</div>
+        <div className="desc-section">Thanks to Apollo's caching store, you can use GraphQL mutations to change your data and see the results reflected in your UI automatically.</div>
         <CodeSnippet operation="mutation" activeLibrary={library} onLibraryChange={this.onLibraryChange} />
       </div>
     );
