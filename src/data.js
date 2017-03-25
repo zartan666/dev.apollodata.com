f32fbd9a8ef6aedaa3ec76f0ed1a982bf59014ed
Sashko Stubailo <sashko@stubailo.com>
2017-03-24 22:22:00 -0700

    Blow up the config

---
+++ src/data.js
@@ -0,0 +1,19 @@
+module.exports = {
+  site: {
+    title: 'Apollo',
+    subtitle: 'Open Source',
+    url_projects_react: 'http://dev.apollodata.com/react',
+    url_projects_reactnative: 'http://dev.apollodata.com/react-native',
+    url_projects_angular: 'http://dev.apollodata.com/angular2',
+    url_projects_swift: 'http://dev.apollodata.com/ios',
+    url_projects_android: 'http://dev.apollodata.com/android',
+    url_projects_server: 'http://dev.apollodata.com/tools',
+    url_projects_vanillajs: 'http://dev.apollodata.com/core',
+    url_community: 'http://dev.apollodata.com/community',
+    url_blog: 'https://dev-blog.apollodata.com/',
+    url_slack: 'http://www.apollodata.com/#slack',
+    url_youtube: 'https://www.youtube.com/c/ApolloGraphQL',
+    twitter_username: 'apollographql',
+    github_username:  'apollographql',
+  }
+}
